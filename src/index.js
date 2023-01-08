c76b2a3adf57f64c5e10a069e15082fbbc988b1a
itsneedtoknow <letta_in_love@mail.ru>
2023-01-08 18:54:49 +0300

    add: add solution to reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+  if(n < 0){
+    n = -(n);
+  }
+    let string = n.toString(); //преобразую число в строку
+    let stringArray = string.split(""); //разбиваю на массив
+    let reversedArray = stringArray.reverse(); // изменяю порядок массива
+    let reversed = reversedArray.join(""); // соединяю
+    let reversedNumber = Number(reversed);  
+    return reversedNumber;//преобразую в число  
+}  
